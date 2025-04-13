8a3a76dfde6d9b92b5d1d24357d7b83969377fc4
Fernando Mondoni <nandomondoni000@hotmail.com>
2025-04-13 17:37:03 -0300

    Add sign in page

---
+++ src/pages/Auth/Auth.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Form, Input, Button, message } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+import '../../App.css';
+
+/* interface LoginFormValues {
+  email: string;
+  password: string;
+} */
+
+const App: React.FC = () => {
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+
+  const onFinish = (/* values: LoginFormValues */) => {
+    console.log('Sign in success');
+    navigate('/home')
+  };
+
+  const onFinishFailed = () => {
+    message.error('Error at sign in. Verify your credentials.');
+  };
+
+  return (
+    <Form
+      form={form}
+      name="login"
+      layout="vertical"
+      initialValues={{ email: '', password: '' }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[
+          { required: true, message: 'Please, input your email!' },
+          { type: 'email', message: 'The email inserted is not valid!' }
+        ]}
+      >
+        <Input placeholder="Input your email" />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please, input your password!' }]}
+      >
+        <Input.Password placeholder="Input your password" />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" block>
+          Sign in
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default App;
