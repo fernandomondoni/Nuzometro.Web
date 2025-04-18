3194113d651d92e9e3b15306081b5bfb1645c71b
Fernando Mondoni <nandomondoni000@hotmail.com>
2025-04-18 01:51:37 -0300

    Add get data in entry login and improvements

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,14 @@
-import React, { useState } from "react";
-import { Layout, Button, message, Select, Card, List, Divider } from "antd";
+import React, { useEffect, useState } from "react";
+import {
+  Layout,
+  Button,
+  message,
+  Select,
+  Card,
+  List,
+  Divider,
+  Spin,
+} from "antd";
 import { registerNu, getNu } from "../../services/nuService";
 import { useNavigate } from "react-router-dom";
 
@@ -20,6 +29,7 @@ const Home: React.FC = () => {
   const [result, setResult] = useState<any>(null);
   const [location, setLocation] = useState<string>(locationOptions[0]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [initialLoading, setInitialLoading] = useState<boolean>(true);
   const [isMuted, setIsMuted] = useState(false);
   const navigate = useNavigate();
 
@@ -28,9 +38,27 @@ const Home: React.FC = () => {
   const playAudio = () => {
     if (isMuted) return;
     const audio = new Audio("/confirma-urna.mp3");
-    //const audio = new Audio("../../../public/confirma-urna.mp3");
     audio.play();
   };
+
+  const fetchResults = async () => {
+    setLoading(true);
+    try {
+      const data = await getNu(username);
+      setResult(data);
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (err) {
+      message.error("Failed to load results.");
+    } finally {
+      setLoading(false);
+      setInitialLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchResults();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleRegisterAndFetchNu = async () => {
     setLoading(true);
@@ -43,11 +71,8 @@ const Home: React.FC = () => {
     try {
       await registerNu(payload);
       message.success("Nu registered successfully!");
-
       playAudio();
-
-      const data = await getNu(username);
-      setResult(data);
+      await fetchResults();
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (err) {
       message.error("Something went wrong while registering or fetching Nu.");
@@ -61,65 +86,85 @@ const Home: React.FC = () => {
       <Content className="home-wrapper scrollable-content">
         <h1 className="title">Nuzometro</h1>
 
-        <div className="select-container">
-          <Select value={location} onChange={setLocation} className="select">
-            {locationOptions.map((loc) => (
-              <Option key={loc} value={loc}>
-                {loc}
-              </Option>
-            ))}
-          </Select>
-        </div>
-
-        <Button
-          type="default"
-          onClick={() => setIsMuted((prev) => !prev)}
-          className="mute-button"
-          style={{ marginBottom: 8 }}
-        >
-          {isMuted ? "Unmute" : "Mute"}
-        </Button>
-
-        <Button
-          type="primary"
-          onClick={handleRegisterAndFetchNu}
-          loading={loading}
-          className="nu-button"
-        >
-          NU
-        </Button>
-
-        {result?.items?.length > 0 && (
-          <div className="results">
-            <Divider>Results</Divider>
-
-            {result.items.map((item: any, index: number) => (
-              <Card key={index} title={item.location} className="result-card">
-                <p>
-                  <strong>User:</strong> {item.username}
-                </p>
-                <p>
-                  <strong>Data:</strong>{" "}
-                  {new Date(item.date).toLocaleDateString("pt-BR")}
-                </p>
-                <p>
-                  <strong>Total:</strong> {item.amount}
-                </p>
-
-                <Divider style={{ margin: "12px 0" }}>Registers</Divider>
-                <List
-                  dataSource={item.recorded_hours}
-                  renderItem={(hour: string) => (
-                    <List.Item>
-                      {new Date(hour).toLocaleString("pt-BR")}
-                    </List.Item>
-                  )}
-                  bordered
-                  size="small"
-                />
-              </Card>
-            ))}
+        {initialLoading ? (
+          <div style={{ textAlign: "center", marginTop: 32 }}>
+            <Spin tip="Loading results..." />
           </div>
+        ) : (
+          <>
+            <div className="select-container">
+              <Select
+                value={location}
+                onChange={setLocation}
+                className="select"
+              >
+                {locationOptions.map((loc) => (
+                  <Option key={loc} value={loc}>
+                    {loc}
+                  </Option>
+                ))}
+              </Select>
+            </div>
+
+            <Button
+              type="default"
+              onClick={() => setIsMuted((prev) => !prev)}
+              className="mute-button"
+              style={{ marginBottom: 8 }}
+            >
+              {isMuted ? "Unmute" : "Mute"}
+            </Button>
+
+            <Button
+              type="primary"
+              onClick={handleRegisterAndFetchNu}
+              loading={loading}
+              className="nu-button"
+            >
+              NU
+            </Button>
+
+            {result?.items?.length > 0 && (
+              <div className="results">
+                <Divider>Results</Divider>
+                {result.items.map((item: any, index: number) => (
+                  <Card
+                    key={index}
+                    title={item.location}
+                    className="result-card"
+                  >
+                    <p>
+                      <strong>User:</strong> {item.username}
+                    </p>
+                    <p>
+                      <strong>Data:</strong>{" "}
+                      {new Date(item.date).toLocaleDateString("pt-BR", {
+                        timeZone: "UTC",
+                      })}
+                    </p>
+                    <p>
+                      <strong>Total:</strong> {item.amount}
+                    </p>
+
+                    <Divider style={{ margin: "12px 0" }}>Registers</Divider>
+                    <List
+                      dataSource={item.recorded_hours}
+                      renderItem={(hour: string) => (
+                        <List.Item>
+                          {new Date(hour)
+                            .toISOString()
+                            .replace("T", ", ")
+                            .substring(0, 20)}
+                        </List.Item>
+                      )}
+                      bordered
+                      size="small"
+                    />
+                  </Card>
+                ))}
+              </div>
+            )}
+          </>
         )}
 
         <Button
