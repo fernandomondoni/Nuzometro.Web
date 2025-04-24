4e8af2fe28f1bf9e0b34ae3f8166bed05f50f1e0
Fernando Mondoni <nandomondoni000@hotmail.com>
2025-04-23 21:08:26 -0300

    Add refresh token endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { refreshToken } from "./authService";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -14,6 +15,29 @@ api.interceptors.request.use(
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        const newAccessToken = await refreshToken();
+
+        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Error refreshing token:", refreshError);
+        window.location.href = "/login";
+      }
+    }
+
+    return Promise.reject(error);
+  }
 );
 
 export default api;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -7,5 +7,33 @@ interface LoginParams {
 
 export const login = async (data: LoginParams) => {
   const response = await api.post("/login", data);
+  const { tokens, username } = response.data;
+
+  localStorage.setItem("access_token", tokens.access_token);
+  localStorage.setItem("id_token", tokens.id_token);
+  localStorage.setItem("refresh_token", tokens.refresh_token);
+  localStorage.setItem("username", username);
+
   return response.data;
+};
+
+export const refreshToken = async () => {
+  const refreshToken = localStorage.getItem("refresh_token");
+  if (!refreshToken) {
+    throw new Error("No refresh token available");
+  }
+
+  try {
+    const response = await api.post("/refresh", {
+      refresh_token: refreshToken,
+    });
+    const { access_token } = response.data.tokens;
+
+    localStorage.setItem("access_token", access_token);
+
+    return access_token;
+  } catch (error) {
+    console.error("Error refreshing token", error);
+    throw error;
+  }
 };
